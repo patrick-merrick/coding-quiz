62fd314a16fcc541141f58c4f04bbfda2eb55e0b
patrick.merrick <patrick.merrick@gmail.com>
2020-04-17 14:35:22 -0700

    Second commit

---
+++ Assets/coding-quiz.js
@@ -0,0 +1,1 @@
+letstatusSpan = document.querySelector("#questions");
---
+++ Assets/questions.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+        title:"what is your favorite color",
+        answers:["blue","red","Green","black"],
+        correct:"red" 
+    },
+    {
+        title:"what is your favorite fuit",
+        answers:["apple","orange","bannana","kiwi"],
+        correct:"kiwi" 
+    } 
+
+]
