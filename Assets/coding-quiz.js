9fde00d8fc0ef44be675d56674c2eedb3e5a7b7f
patrick.merrick <patrick.merrick@gmail.com>
2020-04-17 19:53:36 -0700

    commit 3

--- Assets/coding-quiz.js
+++ Assets/coding-quiz.js
@@ -1,1 +1,33 @@
-letstatusSpan = document.querySelector("#questions");
+let statusSpan = document.querySelector("#questions");
+let BeginQuiz = document.getElementById("startBtn")
+let currentQuesion = 0
+BeginQuiz.addEventListener("click",function(){
+    console.log("it works")
+    question()
+    BeginQuiz.setAttribute("class","hideclass")
+})
+function question(){
+    console.log(questions[currentQuesion]["title"])
+    let questionObject = questions[currentQuesion]
+    let title = document.getElementById("title")
+    let answerList = document.getElementById("answers")
+    answerList.innerHTML="" 
+    title.textContent=questionObject["title"]
+    questionObject["answers"].forEach(function(answer,i) {
+      let answerButton = document.createElement("button")
+      answerButton.textContent=answer
+      answerButton.onclick=answerClick
+      answerButton.setAttribute("value",answer)
+      answerList.appendChild(answerButton)       
+    
+    });
+    currentQuesion++
+
+}
+function answerClick(){
+    console.log(this.value)
+    question()
+    currentQuesion++
+    
+}
+
